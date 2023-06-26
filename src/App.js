51140920b415aa161a50fcbbe2f1099e0859a536
Nuri-I <55015765+Nuri-I@users.noreply.github.com>
2023-06-27 00:37:47 +0300

    connection between front end and back end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './Components/Pages/Login.jsx';
+import Register from './Components/Pages/Register.jsx';
+import Password from './Components/Pages/ForgotPassword.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />}/>
+        <Route Path="register" element={<Register />}/>
+        <Route Path="forgotpassword" element={<Password />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
